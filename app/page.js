1a78d718a104142167dcb31519eeae09f5113ad4
Anirudh Agarwal <andz@Anirudhs-MacBook-Air.local>
2025-11-18 13:10:50 +0530

    updated pages and changed env

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
 import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
-import Testimonials from "./components/homepage/testimonials";
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
 
