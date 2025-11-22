9bbd485f7ce3720d790710ebe76f6a28473d112c
Anirudh Agarwal <andz@Anirudhs-MacBook-Air.local>
2025-11-22 06:02:51 +0530

    changes to navbar and texts

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -20,7 +20,7 @@ function HeroSection() {
         className="absolute -top-[98px] -z-10"
       />
 
-      <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
+      <div className="grid grid-cols-1 items-start lg:grid-cols-[0.51fr_0.49fr] lg:gap-12 gap-y-8">
         <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10 ">
           <h1 className="text-4xl font-bold leading-10 text-black md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
             Hey there, <br />
@@ -139,6 +139,19 @@ function HeroSection() {
                 <span className="text-amber-300">Kubernetes</span>
                 <span className="text-gray-400">{"', '"}</span>
                 <span className="text-amber-300">Docker</span>
+
+                <span className="text-gray-400">{"'],"}</span>
+              </div>
+              <div className="ml-4 lg:ml-8 mr-2">
+                <span className=" text-white">AI skills:</span>
+                <span className="text-gray-400">{`['`}</span>
+                <span className="text-amber-300">LLM</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">RAG</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">Agentic</span>
+                <span className="text-gray-400">{"', '"}</span>
+                <span className="text-amber-300">NLP</span>
 
                 <span className="text-gray-400">{"'],"}</span>
               </div>
